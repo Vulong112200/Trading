349b3e1bf6bbdfaefb4c3e580dc9501a2af5bac3
VuhpAlx <vuhp@allexceed.co.jp>
2026-04-23 23:00:00 +0700

    Trade_bot phase-v4(advise)

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 SYMBOLS = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "PAXGUSDT"]
 # Cập nhật thêm các khung thời gian lớn
 TIMEFRAMES = ["1m", "5m", "15m", "30m", "1h"]
-HISTORY_LIMIT = 1500
+HISTORY_LIMIT = 5000
 MAX_CACHE_SIZE = 5000
